b5c55fc24060491f0cc057a7d6886890db1badb9
giaphust <test@gmail.com>
2026-06-05 01:31:49 +0700

    few

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/todos")
+@RequestMapping("/todos")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "https://frontend-test-seven-pink.vercel.app")
 public class TodoController {
